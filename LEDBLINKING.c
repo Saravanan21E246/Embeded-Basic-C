ebcc699d5d443457bfe9311aee6fed25f0b0ccbe
Saravanan21E246 <127392665+Saravanan21E246@users.noreply.github.com>
2023-03-28 15:31:19 +0530

    Update LEDBLINKING.c

--- LEDBLINKING.c
+++ LEDBLINKING.c
@@ -4,16 +4,16 @@ char i;
 for(i=0;i<=100;i++) /* empty for loop 100 times runing*/
 {}
 }
-void main() /* main function declaration*?
+void main() \* main function declaration*/
 {
 
  	
   	while(1)/* infinite while loop
   	  {
-		P0_1=0;  \* port 0 select to pin 0 high led on *\
-		wait();  *\wait function call to timeing dealay*\
-		P0_1=1;  *\port 0 select to pin 0 high led Off*\
-		wait();  *\wait function call to timeing dealay*\
+		P0_1=0;  /* port 0 select to pin 0 high led on */
+		wait();  /*wait function call to timeing dealay*/
+		P0_1=1;  /*port 0 select to pin 0 high led Off*/
+		wait();  /*wait function call to timeing dealay*/
   	  }
 
 }

09fb7994817bc53f477ed477518d3837248aba34
Saravanan21E246 <127392665+Saravanan21E246@users.noreply.github.com>
2023-03-28 15:29:08 +0530

    Create LEDBLINKING.c

---
+++ LEDBLINKING.c
@@ -0,0 +1,19 @@
+#include <AT89S52.h> /*header fille*/
+void wait(){ /* delay user define not return function */
+char i;
+for(i=0;i<=100;i++) /* empty for loop 100 times runing*/
+{}
+}
+void main() /* main function declaration*?
+{
+
+ 	
+  	while(1)/* infinite while loop
+  	  {
+		P0_1=0;  \* port 0 select to pin 0 high led on *\
+		wait();  *\wait function call to timeing dealay*\
+		P0_1=1;  *\port 0 select to pin 0 high led Off
+		wait();  *\wait function call to timeing dealay*\
+  	  }
+
+}
